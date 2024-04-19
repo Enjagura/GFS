cea42ec7c76094b097f7fb4030e054daf578ce5b
Enjagura <114489232+Enjagura@users.noreply.github.com>
2024-04-19 09:05:42 +0200

    Create SquarePeg.java

---
+++ java/adapter/square/SquarePeg.java
@@ -0,0 +1,29 @@
+package adapter.square;
+
+/**
+*  Quadratische Objekte sind nicht kompatibel mit runden Objekten.
+*  Diese sollen jedoch miteinander kompatibel gemacht werden.
+**/
+
+public class SquarePeg 
+{
+    private double width;
+
+    public SquarePeg(double width) 
+    {
+        this.width = width;
+    }
+
+    public double getWidth() 
+    {
+        return width;
+    }
+
+    public double getSquare() 
+    {
+        double result;
+        
+        result = Math.pow(this.width, 2);
+        return result;
+    }
+}
