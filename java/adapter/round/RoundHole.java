81c78168433c45db7cb352de27aff9905c74f6b6
Enjagura <114489232+Enjagura@users.noreply.github.com>
2024-04-19 09:09:22 +0200

    Create RoundHole.java

---
+++ java/adapter/round/RoundHole.java
@@ -0,0 +1,28 @@
+package adapter.round;
+
+/**
+ *   Runde Löcher sind kompatibel mit runden Objekten
+**/
+
+public class RoundHole 
+{
+    private double radius;
+
+    public RoundHole(double radius) 
+    {
+        this.radius = radius;
+    }
+
+    public double getRadius() 
+    {
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg) 
+    {
+        boolean result;
+        
+        result = (this.getRadius() >= peg.getRadius());
+        return result;
+    }
+}
